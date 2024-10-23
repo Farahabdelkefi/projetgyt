a03fcacd9d24dbe907d0fb86f946553d7f3b161c
Farahabdelkefi <farahabdelkefi5@gmail.com>
2024-10-23 15:48:19 +0200

    Update Node.js

--- Node.js
+++ Node.js
@@ -2,8 +2,9 @@ const	http	=	require('http');
 const	server	=	http.createServer((req,	res)	=>	{
 				res.statusCode	=	200;
 				res.setHeader('Content-Type',	'text/plain');
-				res.end('Hello	World\n');
+				res.end('Hello	Azure!\n');
 });
-server.listen(3000,	()	=>	{
-				console.log('Server	running	at	http://localhost:3000/');
+const	port	=	process.env.PORT	||	3000;
+server.listen(port,	()	=>	{
+				console.log(`Server	running	at	http://localhost:${port}/`);
 });
